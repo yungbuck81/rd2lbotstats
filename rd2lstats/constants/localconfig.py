e977c2eede922e74e9a5db11020227f390204596
Joe Dobrow <joedobrow@MacBook-Pro.local>
2024-02-03 04:30:38 -0600

    multiple admin_ids

--- rd2lstats/constants/localconfig.py
+++ rd2lstats/constants/localconfig.py
@@ -1,1 +1,5 @@
-admin_id = 215689154820833281 #discord ID
+#discordIds (make sure developer mode is on, go to settings, my account, and then click the 3 dots by the username
+ADMIN_IDS = [
+    215689154820833281, 
+    195063835416199168
+]
