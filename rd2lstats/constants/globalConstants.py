4d412e6cfd38e29d79160011d28e48cfa3235fde
yungbuck81 <bradbarrett96@gmail.com>
2024-02-04 17:35:53 -0800

    Updated global constants to include player ranking cutoff for leaderboard count

--- rd2lstats/constants/globalConstants.py
+++ rd2lstats/constants/globalConstants.py
@@ -1,10 +1,11 @@
 
 # Contains the current week match ID's to parse stats for
-match_ids = ["7559544283", "7559577949", "7559544528", "7559578126", "7559545532", "7559582317", "7559543389", "7559574826"]#, "7436219127", "7436254888"]#, "7419232570", "7419309960"]#, "7351050181", "7351080912"]#, "7216201177", "7216234994"#, "7206400107", "7206434646"]
-current_week = 2
+match_ids = ["7549089946", "7549125560", "7549082146", "7549122501", "7549078176", "7549125091", "7549082268", "7549126413"]#, "7436219127", "7436254888"]#, "7419232570", "7419309960"]#, "7351050181", "7351080912"]#, "7216201177", "7216234994"#, "7206400107", "7206434646"]
+current_week = 1
 
 # permissionkey location - added in manually
 permissionkeyfile = "rd2lstats\constants\permissionkey.txt"
+player_ranking_cutoff = 8
 
 # Used for fetching Hero icon images in embeds
 steam_cdn = "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/"
@@ -32,36 +33,36 @@ fantasy_first_blood = 4
 fantasy_stuns_multiplier = 0.05
 
 # Pos 1 constants
-pos1directory = "rd2lbotstats/rd2lstats/pos1stats/"
-pos1currentdirectory = "rd2lbotstats/rd2lstats/current_week_stats/pos1stats/"
+pos1directory = "rd2lstats/pos1stats/"
+pos1currentdirectory = "rd2lstats/current_week_stats/pos1stats/"
 pos1gpmfile = "pos1gpmLeaderboard.csv"
 pos1kdafile = "pos1kdaLeaderboard.csv"
 pos1fantasyfile = "pos1fantasyLeaderboard.csv"
 
 # Pos 2 constants
-pos2directory = "rd2lbotstats/rd2lstats/pos2stats/"
-pos2currentdirectory = "rd2lbotstats/rd2lstats/current_week_stats/pos2stats/"
+pos2directory = "rd2lstats/pos2stats/"
+pos2currentdirectory = "rd2lstats/current_week_stats/pos2stats/"
 pos2gpmfile = "pos2gpmLeaderboard.csv"
 pos2kdafile = "pos2kdaLeaderboard.csv"
 pos2fantasyfile = "pos2fantasyLeaderboard.csv"
 
 # Pos 3 constants
-pos3directory = "rd2lbotstats/rd2lstats/pos3stats/"
-pos3currentdirectory = "rd2lbotstats/rd2lstats/current_week_stats/pos3stats/"
+pos3directory = "rd2lstats/pos3stats/"
+pos3currentdirectory = "rd2lstats/current_week_stats/pos3stats/"
 pos3gpmfile = "pos3gpmLeaderboard.csv"
 pos3kdafile = "pos3kdaLeaderboard.csv"
 pos3fantasyfile = "pos3fantasyLeaderboard.csv"
 
 # Pos 4 constants
-pos4directory = "rd2lbotstats/rd2lstats/pos4stats/"
-pos4currentdirectory = "rd2lbotstats/rd2lstats/current_week_stats/pos4stats/"
+pos4directory = "rd2lstats/pos4stats/"
+pos4currentdirectory = "rd2lstats/current_week_stats/pos4stats/"
 pos4gpmfile = "pos4gpmLeaderboard.csv"
 pos4kdafile = "pos4kdaLeaderboard.csv"
 pos4fantasyfile = "pos4fantasyLeaderboard.csv"
 
 # Pos 5 constants
-pos5directory = "rd2lbotstats/rd2lstats/pos5stats/"
-pos5currentdirectory = "rd2lbotstats/rd2lstats/current_week_stats/pos5stats/"
+pos5directory = "rd2lstats/pos5stats/"
+pos5currentdirectory = "rd2lstats/current_week_stats/pos5stats/"
 pos5gpmfile = "pos5gpmLeaderboard.csv"
 pos5kdafile = "pos5kdaLeaderboard.csv"
 pos5fantasyfile = "pos5fantasyLeaderboard.csv"
